dfa324edae2a0da8c558be9aef580dc4e20344e9
Steve Schwab <schwabs@umich.edu>
2024-04-16 13:18:13 -0700

    import nav components

--- portfolio-piece-deployed-on-vercel/src/App.js
+++ portfolio-piece-deployed-on-vercel/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import Craftsmanship from './components/Craftsmanship';
+import Reviews from './components/Reviews';
+import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -10,6 +14,10 @@ export default function App() {
     <div>
       <Header />
       <Routes>
+        <Route path="/contact" element={<ContactForm />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/our-craftsmanship" element={<Craftsmanship />} />
+        <Route path="/about-us" element={<AboutUs />} />
         <Route path="/" exact element={<Home />} />
       </Routes>
       <Footer />
